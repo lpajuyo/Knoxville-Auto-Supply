53e014a835f91319c8d156091fcd63917c2bc38a
aflormata2000@gmail.com <aflormata2000@gmail.com>
2017-09-28 20:57:48 +0800

    add_orders/js not working

--- application/controllers/knoxville.php
+++ application/controllers/knoxville.php
@@ -201,9 +201,10 @@ class Knoxville extends CI_Controller {
         }
         else{
 
-	 $orderRecord=array('clientID'=>$_POST['clientid'],'date'=>$_POST['date'],'time'=>$_POST['time'],'due'=>$_POST['duedate'],'userID'=>$this->session->userdata('userID'));
-	 $this->Order->create($orderRecord);
-	 
+		 $orderRecord=array('clientID'=>$_POST['clientid'],'date'=>$_POST['date'],'time'=>$_POST['time'],'due'=>$_POST['duedate'],'userID'=>$this->session->userdata('userID'));
+		 $this->Order->create($orderRecord);
+		 redirect('knoxville/viewOrders');
+			}
 		}
 	
 	public function viewOrders(){
