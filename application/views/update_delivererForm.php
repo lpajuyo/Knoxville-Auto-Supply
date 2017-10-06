49ab592653b3415bf0787fe7366c9c13fe67439d
Leonardo Pajuyo <pajuyo.leo@gmail.com>
2017-10-06 21:22:58 +0800

    made delete transaction, fixed edit deliverer

--- application/views/update_delivererForm.php
+++ application/views/update_delivererForm.php
@@ -6,23 +6,27 @@
   <?php echo form_open('knoxville/updateDeliverer/'.$delivererID); //this is equal to <form method="post" accept-charset="utf-8" action="http://localhost/Knoxville-Auto-Supply/knoxville/addClient">
                                      //to add attributes, edit to: echo form('knoxville/addClient','class="lala" id="lala"'); 
   ?> 
-  <div class="ClientForm">
+   <h2 class="heading text-center">UPDATE DELIVERER</h2>
+  <div class=" container ClientForm">
+ 
     <div>
-    <label for="vehicle">Vehicle:</label>
-    <input class="form-control"  type="text" name="vehicle" id="vehicle" value="<?php echo $vehicle?>" />
+    <label class="control-label" for="vehicle">Vehicle:</label>
+    <input class="form-control"type="text" name="vehicle" id="vehicle" value="<?php echo $vehicle; ?>" />
     </div>
     
     <div>
-    <label for="cnum">Contact Number:</label>
-    <input class="form-control"  type="text" name="cnum" id="cnum" value="<?php echo $cnum?>" />
+    <label class="control-label" for="cnum">Contact Number:</label>
+    <input class="form-control"type="text" name="cnum" id="cnum" value="<?php echo $cnum; ?>" />
     </div>
     
-    <div class="col-sm-6 text-center top">
-    <label for="assigned">Assigned?</label>
-    <input class="form-control"  type="checkbox" name="assigned" id="assigned" <?php echo $assigned?> />
-    </div>
-    <div class="col-sm-4 submarg">
-    <input class="form-control sub-btn"type="submit" value="Submit" />
+    <div class="top col-sm-6 top text-center">
+    <label class="control-label" for="assigned">Assigned personnel
+	   <input class="form-control"type="text" name="assigned" id="assigned" value="<?php echo $assigned; ?>" />
+	</div>
+	<div class="submarg col-sm-4">
+    <input class="form-control sub-btn" type="submit" value="SUBMIT"/>
+	</div>
   </form>
- </div>
+  </div>
 </body>
+</html>
