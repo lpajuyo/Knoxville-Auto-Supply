87d40a82c2a6dad02479ed12854b327ab06d2c1b
Leonardo Pajuyo <pajuyo.leo@gmail.com>
2017-10-06 21:21:45 +0800

    made shipment model

---
+++ application/models/shipment_model.php
@@ -0,0 +1,33 @@
+<?php
+class Shipment_model extends CI_Model {
+    private $table = 'shipment';
+    
+    function create($delivererRecord){
+        $this->db->insert($this->table, $delivererRecord);
+    }
+    
+    function read($condition=null){
+        $this->db->select('*');
+        $this->db->from($this->table);
+        
+        if(isset($condition))
+            $this->db->where($condition);
+        
+        $query = $this->db->get();
+        
+        if($query->num_rows()>0)
+            return $query->result_array();
+        else
+            return false;
+        
+    }
+    
+    function update($newRecord){
+        $this->db->replace($this->table,$newRecord);
+    }
+    
+    function del($where_array){
+        $this->db->delete($this->table,$where_array);
+    }
+}
+?>
