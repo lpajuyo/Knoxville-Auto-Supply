938c4342fc05f3eaed5f2a1e32665b49c4d7cf00
unknown <alexiaarmando04@gmail.com>
2017-10-07 23:17:33 +0800

    Updated views

--- application/views/update_sales_agentForm.php
+++ application/views/update_sales_agentForm.php
@@ -1,53 +1,61 @@
 <!DOCTYPE html>
 
-<body>
+<div class="tab-content">
+
   <?php echo validation_errors(); ?>
   
   <?php echo form_open('knoxville/updateSalesAgent'); //this is equal to <form method="post" accept-charset="utf-8" action="http://localhost/Knoxville-Auto-Supply/knoxville/addClient">
                                      //to add attributes, edit to: echo form('knoxville/addClient','class="lala" id="lala"'); 
   ?> 
-  <div class="ClientForm">
+  <div>&nbsp;</div>
+  <h2 class="text-center">UPDATE SALES AGENT</h2>
+  <div class="container ClientForm">
     <div>
-    <label for="userID">UserID:</label>
-    <input class="form-control" type="text" name="userID" id="userID" value="<?php echo $userID?>" readonly />
+		<label class="control-label col-sm-4" for="userID">UserID:</label>
+		<input class="form-control" type="text" name="userID" id="userID" value="<?php echo $userID?>" readonly />
     </div>
     
     <div>
-    <label for="pass">Password:</label>
-    <input class="form-control" type="text" name="pass" id="pass" value="<?php echo $pass?>" />
+		<label class="control-label col-sm-4" for="pass">Password:</label>
+		<input class="form-control col-sm-4" type="password" name="pass" id="pass" value="<?php echo $pass?>" />
     </div>
     
     <div>
-    <label for="name">Name:</label>
-    <input class="form-control"  type="text" name="name" id="name" value="<?php echo $name?>" />
+		<label class="control-label col-sm-4" for="name">Name:</label>
+		<input class="form-control col-sm-4" type="text" name="name" id="name" value="<?php echo $name?>" />
     </div>
     
     <div>
-    <label for="bday">Birthdate:</label>
-    <input class="form-control"  type="date" name="bday" id="bday" value="<?php echo $bday?>" />
+		<label class="control-label col-sm-4" for="bday">Birthdate:</label>
+		<input class="form-control col-sm-4" type="date" name="bday" id="bday" value="<?php echo $bday?>" />
     </div>
     
     <div>
-    <label for="age">Age:</label>
-    <input class="form-control"  type="text" name="age" id="age" value="<?php echo $age?>" />
+		<label class="control-label col-sm-4" for="age">Age:</label>
+		<input class="form-control col-sm-4" type="text" name="age" id="age" value="<?php echo $age?>" />
     </div>
     
     <div>
-    <label for="email">Email:</label>
-    <input class="form-control"  type="email" name="email" id="email" value="<?php echo $email?>" />
+		<label class="control-label col-sm-4" for="email">Email:</label>
+		<input class="form-control col-sm-4" type="email" name="email" id="email" value="<?php echo $email?>" />
     </div>
     
     <div>
-    <label for="cnum">Contact Number:</label>
-    <input class="form-control"  type="text" name="cnum" id="cnum" value="<?php echo $cnum?>" />
+		<label class="control-label col-sm-4" for="cnum">Contact Number:</label>
+		<input class="form-control col-sm-4" type="text" name="cnum" id="cnum" value="<?php echo $cnum?>" />
     </div>
     
-    <div class="col-sm-6 text-center top">
-    <label for="isAdmin">Admin?</label>
-    <input class="form-control"  type="checkbox" name="isAdmin" id="isAdmin" <?php echo $isAdmin?> />
-    </div>
-    <div class="col-sm-4 submarg">
-    <input class="form-control sub-btn"type="submit" value="Submit" />
-  </form>
+    <div class="col-sm-4">
+		<label for="isAdmin">Admin?</label>
+		<input class="check"  type="checkbox" name="isAdmin" id="isAdmin" <?php echo $isAdmin?> />
+	</div>
+    <div class="col-sm-6">
+		<input class="subUpdate" type="submit" value="SUBMIT" />
+	</div>
+	
+	</form>
+	</div>
+</div>
  </div>
 </body>
+</html>
