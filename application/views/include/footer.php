9201a5a6fcbaad0789619220d6c7f5f7003dadf7
aflormata01 <aflormata2000@gmail.com>
2018-03-15 03:43:59 +0800

    table sorter

---
+++ application/views/include/footer.php
@@ -0,0 +1,5 @@
+<footer>
+
+<script src="<?php echo base_url('assets/js/tablesorter.js'); ?>"></script>
+</footer>
+</html>
--- application/views/include/header.php
+++ application/views/include/header.php
@@ -10,8 +10,10 @@
 	<script src="<?php echo base_url('bootstrap/js/jquery.min.js'); ?>"></script>
 	<script src="<?php echo base_url('bootstrap/js/bootstrap.min.js'); ?>"></script>
   <script src="<?php echo base_url('assets/js/multiple_selection.js'); ?>"></script>
-  <script src="<?php echo base_url('assets/js/search.js'); ?>"></script>
+<script src="<?php echo base_url('assets/js/search.js'); ?>"></script>
   <script src="<?php echo base_url('assets/js/select.js'); ?>"></script>
+ 
+ 
 	
 </head>
 <body>
